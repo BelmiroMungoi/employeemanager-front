4f7f2bb83b3cb36f2cbaa1eca8eb59aacf9b91de
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-01-28 15:50:17 +0200

    adding methods in the service

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  private apiServerUrl = '';
+
+  constructor(private http: HttpClient) {
+  }
+
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(this.apiServerUrl + '/employee/');
+  }
+
+  public getEmployeeById(id: any): Observable<Employee> {
+    return this.http.get<Employee>(this.apiServerUrl + '/employee/find/' + id);
+  }
+
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(this.apiServerUrl + '/employee/', employee)
+  }
+
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(this.apiServerUrl + '/employee/', employee)
+  }
+
+  public deleteEmployee(id: any): Observable<any> {
+    return this.http.delete(this.apiServerUrl + '/employee/' + id, { responseType: 'text' })
+  }
 }
