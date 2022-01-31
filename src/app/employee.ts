8ff20f82539c6b00fe66881796ab2d4a6348a66a
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-01-31 17:48:13 +0200

    updating  a model class

--- src/app/employee.ts
+++ src/app/employee.ts
@@ -1,10 +1,10 @@
-export interface Employee {
-    id: number;
-    name: string;
-    email: string;
-    jobTitle: string;
-    phone: string;
-    image: string;
-    employeeCode: string;
+export class Employee {
+    id!: number;
+    name!: string;
+    email!: string;
+    jobTitle!: string;
+    phone!: string;
+    image!: string;
+    employeeCode!: string;
 
 }
